6ffc4575ed6d86da4215c14dc24d0580c5981a4c
Sadiq Ibrahim <yndaski@gmail.com>
2026-04-27 14:58:59 +0100

    first commit

---
+++ src/main/java/com/golivebackend/GoliveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.golivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoliveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoliveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golivebackend/GoliveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoliveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
